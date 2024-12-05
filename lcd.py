208b9b1de4500fd5240318cd8229084f4efe10d2
vj6yc13 <vj6yc13@naver.com>
2024-12-05 15:51:53 +0900

    Update lcd.py

--- lcd.py
+++ lcd.py
@@ -123,10 +123,10 @@ if __name__ == "__main__":
     try:
         lcd_init()
         while True:
-            lcd_string("hey", LCD_LINE_1)
+            lcd_string("♬♪hey", LCD_LINE_1)
             lcd_string("hihi", LCD_LINE_2)
             time.sleep(3)
-            lcd_string("byebye", LCD_LINE_1)
+            lcd_string("byebye♬♪", LCD_LINE_1)
             lcd_string("Goodbye!", LCD_LINE_2)
             time.sleep(3)
     except KeyboardInterrupt:
